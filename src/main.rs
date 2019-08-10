7bd4bc42387cd6f7f3dd8349b2ae47b0f576be4b
Graham Breed <gbreed@gmail.com>
2019-08-10 22:45:00 +0100

    Rough and ready Eratosthenes sieve

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("One day this will do something");
+    let mut primes = primes_below(50);
+    for p in &mut primes {
+        println!("{} is prime", p);
+    }
+}
+
+fn primes_below(n: usize) -> Vec<usize> {
+    let mut hasfactors: Vec<bool> = Vec::new();
+    for _ in 2..n {
+        hasfactors.push(false);
+    }
+    for i in 2..(n/2) {
+        if !hasfactors[i - 2] {
+            let mut j = 2 * i;
+            while j < n {
+                hasfactors[j - 2] = true;
+                j += i;
+            }
+        }
+    }
+    let mut result: Vec<usize> = Vec::new();
+    for i in 2..n {
+        if !hasfactors[i - 2] {
+            result.push(i);
+        }
+    }
+    result
 }
