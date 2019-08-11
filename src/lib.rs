4ab21de9990e47aaaaf702fc5c80641ba6dcc1fe
Graham Breed <gbreed@gmail.com>
2019-08-11 18:50:43 +0100

    Move the functions to 'regular' top-level
    This matches the way regutils works in Python:
    other modules can use it.

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,30 @@
-pub mod regutils {
-    pub fn prime_limit(n: u16) -> Vec<(u16, f64)> {
-        let mut result: Vec<(u16, f64)> = Vec::new();
-        for p in primes_below(n + 1) {
-            result.push((p, cents(p as f64)));
-        }
-        result
+pub fn prime_limit(n: u16) -> Vec<(u16, f64)> {
+    let mut result: Vec<(u16, f64)> = Vec::new();
+    for p in primes_below(n + 1) {
+        result.push((p, cents(p as f64)));
     }
+    result
+}
 
-    pub fn cents(ratio: f64) -> f64 {
-        ratio.log2() * 12e2
-    }
+pub fn cents(ratio: f64) -> f64 {
+    ratio.log2() * 12e2
+}
 
-    fn primes_below(n: u16) -> Vec<u16> {
-        let n = n as usize;
-        let mut hasfactors = vec![false; n - 2];
-        let mut result: Vec<u16> = Vec::new();
-        for i in 2..n {
-            if !hasfactors[i - 2] {
-                result.push(i as u16);
-                let mut j = i;
-                while {
-                    j += i;
-                    j < n
-                } {
-                    hasfactors[j - 2] = true;
-                }
+fn primes_below(n: u16) -> Vec<u16> {
+    let n = n as usize;
+    let mut hasfactors = vec![false; n - 2];
+    let mut result: Vec<u16> = Vec::new();
+    for i in 2..n {
+        if !hasfactors[i - 2] {
+            result.push(i as u16);
+            let mut j = i;
+            while {
+                j += i;
+                j < n
+            } {
+                hasfactors[j - 2] = true;
             }
         }
-        result
     }
+    result
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use regular::regutils::prime_limit;
+use regular::prime_limit;
 
 fn main() {
     for (n, x) in prime_limit(50) {
