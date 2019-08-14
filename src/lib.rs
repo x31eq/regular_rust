e4948aa2b01780ab9ff13c85cb814274a70130f5
Graham Breed <gbreed@gmail.com>
2019-08-14 21:09:07 +0100

    Revert "Use a closure for the primes calculation to save specifying the types"
    That was a nice idea but the function could also have been inlined.
    Let's leave it internally accessible
    
    This reverts commit 3221288d061c2c774d46691a88f8b66958196fe1.

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,4 @@
 pub fn prime_limit(n: u16) -> Vec<(u16, f64)> {
-    let primes_below = |n| {
-        let n = n as usize;
-        let mut hasfactors = vec![false; n - 2];
-        let mut result: Vec<u16> = Vec::new();
-        for i in 2..n {
-            if !hasfactors[i - 2] {
-                result.push(i as u16);
-                let mut j = i;
-                while {
-                    j += i;
-                    j < n
-                } {
-                    hasfactors[j - 2] = true;
-                }
-            }
-        }
-        result
-    };
-
     primes_below(n + 1).iter()
         .map(|p| (*p, cents(*p as f64)))
         .collect()
@@ -25,6 +6,25 @@ pub fn prime_limit(n: u16) -> Vec<(u16, f64)> {
 
 pub fn cents(ratio: f64) -> f64 {
     ratio.log2() * 12e2
+}
+
+fn primes_below(n: u16) -> Vec<u16> {
+    let n = n as usize;
+    let mut hasfactors = vec![false; n - 2];
+    let mut result: Vec<u16> = Vec::new();
+    for i in 2..n {
+        if !hasfactors[i - 2] {
+            result.push(i as u16);
+            let mut j = i;
+            while {
+                j += i;
+                j < n
+            } {
+                hasfactors[j - 2] = true;
+            }
+        }
+    }
+    result
 }
 
 #[cfg(test)]
