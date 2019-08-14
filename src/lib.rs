62895f05f37a32bc25d3a7b6ec6459925487e18c
Graham Breed <gbreed@gmail.com>
2019-08-14 21:49:55 +0100

    Iterate the prime finding as well

--- src/lib.rs
+++ src/lib.rs
@@ -9,22 +9,25 @@ pub fn cents(ratio: f64) -> f64 {
 }
 
 fn primes_below(n: u16) -> Vec<u16> {
-    let n = n as usize;
-    let mut hasfactors = vec![false; n - 2];
-    let mut result: Vec<u16> = Vec::new();
-    for i in 2..n {
-        if !hasfactors[i - 2] {
-            result.push(i as u16);
+    let top = n as usize;
+    let mut hasfactors = vec![false; top - 2];
+    (2..n).filter(|i| {
+        let i = *i as usize;
+        if hasfactors[i - 2] {
+            false
+        }
+        else {
             let mut j = i;
             while {
                 j += i;
-                j < n
+                j < top
             } {
                 hasfactors[j - 2] = true;
             }
+            true
         }
-    }
-    result
+    })
+    .collect()
 }
 
 #[cfg(test)]
