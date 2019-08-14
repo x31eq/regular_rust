7f21991571da411139eef3d500fc131fcba40f27
Graham Breed <gbreed@gmail.com>
2019-08-14 21:14:56 +0100

    Test the prime generation

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,7 @@ fn primes_below(n: u16) -> Vec<u16> {
 
 #[cfg(test)]
 mod tests {
-    use super::{cents,prime_limit};
+    use super::*;
 
     #[test]
     fn octave_cents() {
@@ -44,5 +44,14 @@ mod tests {
                 .map(|(p, _size)| *p)
                 .collect();
         assert_eq!(primes, vec![2, 3, 5, 7]);
+    }
+
+    #[test]
+    fn small_primes() {
+        let from_pari = vec![
+            2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31,
+            37, 41, 43, 47, 53, 59, 61, 67, 71, 73,
+            79, 83, 89, 97];
+        assert_eq!(primes_below(100), from_pari);
     }
 }
