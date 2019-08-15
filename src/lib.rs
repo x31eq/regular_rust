9ee248cc4aa446e0853cc233e8d2a18d25672bfa
Graham Breed <gbreed@gmail.com>
2019-08-15 22:06:10 +0100

    Unreference the partials

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,11 @@ impl PrimeLimit {
         PrimeLimit{ numbers: prime_numbers, pitches: plimit }
     }
 
-    pub fn partials(&self) -> Vec<(&u16, &f64)> {
-        self.numbers.iter().zip(&self.pitches).collect()
+    pub fn partials(&self) -> Vec<(u16, f64)> {
+        self.numbers.iter()
+            .zip(&self.pitches)
+            .map(|(n, p)| (*n, *p))
+            .collect()
     }
 }
 
