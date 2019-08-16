712393bfe812739f18e908e17374753c8e182716
Graham Breed <gbreed@gmail.com>
2019-08-16 19:21:51 +0100

    Use types

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,36 @@
+type Cents = f64;
+// Irrational ratio
+type Irratio = f64;
+// Human hearing covers about 10 octaves,
+// which means 11 bits.
+type Harmonic = u16;
+
 pub struct PrimeLimit {
-    pub numbers: Vec<u16>,
-    pub pitches: Vec<f64>,
+    pub numbers: Vec<Harmonic>,
+    pub pitches: Vec<Cents>,
 }
 
 impl PrimeLimit {
-    pub fn new(n: u16) -> PrimeLimit {
+    pub fn new(n: Harmonic) -> PrimeLimit {
         let prime_numbers = primes_below(n + 1);
         let plimit = prime_numbers.iter()
-                        .map(|p| cents(*p as f64))
+                        .map(|p| cents(*p as Irratio))
                         .collect();
         PrimeLimit{ numbers: prime_numbers, pitches: plimit }
     }
 
-    pub fn partials(&self) -> Vec<(u16, f64)> {
+    pub fn partials(&self) -> Vec<(Harmonic, Cents)> {
         self.numbers.iter().cloned()
             .zip(self.pitches.iter().cloned())
             .collect()
     }
 }
 
-pub fn cents(ratio: f64) -> f64 {
+pub fn cents(ratio: Irratio) -> Cents {
     ratio.log2() * 12e2
 }
 
-fn primes_below(n: u16) -> Vec<u16> {
+fn primes_below(n: Harmonic) -> Vec<Harmonic> {
     let top = n as usize;
     let mut hasfactors = vec![false; top - 2];
     (2..n).filter(|i| {
