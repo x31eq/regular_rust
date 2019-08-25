218e5bd461106f63412dab13d7d0e452caeefada
Graham Breed <gbreed@gmail.com>
2019-08-25 17:49:50 +0100

    Binary takes command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,19 @@
 fn main() {
-    let limit = regular::PrimeLimit::new(50);
-    for (n, x) in limit.partials() {
-        println!("{}: {}", n, x);
-    }
-    println!("11-limit");
-    let limit11 = regular::PrimeLimit::new(11).pitches;
-    for mapping in regular::cangwu::get_equal_temperaments(
-            &limit11, 1.0, 10) {
-        println!("{:?}", mapping);
-    }
-    println!("11-limit 1-cent badness of 31-equal: {}",
-             regular::cangwu::equal_temperament_badness(
-                &limit11, 1.0, &vec![31, 49, 72, 87, 107]));
-    let big_limit = regular::PrimeLimit::new(200);
+    let args: Vec<String> = std::env::args().collect();
+    let n_results: usize = args[1].parse().unwrap();
+    let prime_limit: regular::Harmonic = args[2].parse().unwrap();
+
+    let limit = regular::PrimeLimit::new(prime_limit);
     let mappings = regular::cangwu::get_equal_temperaments(
-            &big_limit.pitches, 0.3, 10);
+            &limit.pitches, 0.3, n_results);
     println!("{}-limit",
-             big_limit.numbers[big_limit.numbers.len() - 1]);
-    println!("{:?}", mappings.iter()
-                        .map(|m| m[0])
-                        .collect::<Vec<_>>());
-    println!("Badness of worst in the list {:?}",
-             regular::cangwu::equal_temperament_badness(
-                &big_limit.pitches,
+             limit.numbers[limit.numbers.len() - 1]);
+    let badness = regular::cangwu::equal_temperament_badness(
+                &limit.pitches,
                 0.3,
-                &mappings[mappings.len() - 1]));
+                &mappings[mappings.len() - 1]);
+    for et in mappings {
+        println!("{:?}", et);
+    }
+    println!("Badness of worst in the list {:?}", badness);
 }
