6ffdf45c4234925b1ffb9d4bbc159e5749895b24
Graham Breed <gbreed@gmail.com>
2019-08-31 09:14:53 +0100

    Specify the badness parameter on the command line

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,16 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     let n_results: usize = args[1].parse().unwrap();
     let prime_limit: regular::Harmonic = args[2].parse().unwrap();
+    let ek: regular::Cents = args[3].parse().unwrap();
 
     let limit = regular::PrimeLimit::new(prime_limit);
     let mappings = regular::cangwu::get_equal_temperaments(
-            &limit.pitches, 0.3, n_results);
+            &limit.pitches, ek, n_results);
     println!("{}-limit",
              limit.numbers[limit.numbers.len() - 1]);
     let badness = regular::cangwu::equal_temperament_badness(
                 &limit.pitches,
-                0.3,
+                ek,
                 &mappings[mappings.len() - 1]);
     for et in mappings {
         println!("{:?}", et);
