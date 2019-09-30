ea8b3e7ff5a65d5c3a40d225bc4fea7ac22ab962
Graham Breed <gbreed@gmail.com>
2019-09-30 21:18:03 +0100

    Take out the threading support
    It won't work with Web Assembly anyway, and I don't think
    it was doing much on the web server.

--- src/cangwu.rs
+++ src/cangwu.rs
@@ -5,10 +5,6 @@ use na::{DMatrix, DVector};
 
 use super::{Cents, ETMap, FactorElement, Mapping, PriorityQueue, Tuning};
 use std::collections::HashSet;
-use std::sync::{Arc, RwLock};
-use std::thread;
-
-static N_THREADS: i32 = 4;
 
 pub struct TemperamentClass {
     plimit: DVector<Cents>,
@@ -160,34 +156,19 @@ pub fn get_equal_temperaments(
     let plimit: Tuning =
         plimit.iter().map(|p| 12e2 * (p / plimit[0])).collect();
 
-    let results = Arc::new(RwLock::new(PriorityQueue::new(n_results)));
-    let mut children = Vec::with_capacity(N_THREADS as usize);
+    let mut results = PriorityQueue::new(n_results);
     let bmax = preliminary_badness(&plimit, ek, n_results);
-    let plimit = Arc::new(plimit);
-    for thread_id in 0..N_THREADS {
-        let results = Arc::clone(&results);
-        let plimit = Arc::clone(&plimit);
-        let child = thread::spawn(move || {
-            let mut n_notes = 1 + thread_id;
-            let mut cap = bmax;
-            while (f64::from(n_notes)) < cap / ek {
-                for mapping in limited_mappings(n_notes, ek, cap, &plimit) {
-                    let mut results = results.write().unwrap();
-                    let bad =
-                        equal_temperament_badness(&plimit, ek, &mapping);
-                    results.push(bad, mapping.clone());
-                }
-                n_notes += N_THREADS;
-                cap = cap.min(results.read().unwrap().cap);
-            }
-        });
-        children.push(child);
-    }
-    for child in children {
-        child.join().expect("Threading error");
+    let mut n_notes = 1;
+    let mut cap = bmax;
+    while (f64::from(n_notes)) < cap / ek {
+        for mapping in limited_mappings(n_notes, ek, cap, &plimit) {
+            let bad = equal_temperament_badness(&plimit, ek, &mapping);
+            results.push(bad, mapping.clone());
+        }
+        n_notes += 1;
+        cap = cap.min(results.cap);
     }
 
-    let mut results = results.write().expect("Couldn't extract results");
     debug_assert!(results.len() == n_results);
     results.extract()
 }
