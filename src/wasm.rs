02a2a783fa0a68319a531aa4704143c93ebf0e97
Graham Breed <gbreed@gmail.com>
2019-10-12 18:47:26 +0100

    Adjusted parameter and no initial search

--- src/wasm.rs
+++ src/wasm.rs
@@ -3,22 +3,18 @@ use wasm_bindgen::prelude::{wasm_bindgen, JsValue};
 use super::cangwu;
 use super::{Cents, Harmonic, PrimeLimit};
 
-// Called when the wasm module is instantiated
-#[wasm_bindgen(start)]
-pub fn wasm_main() -> Result<(), JsValue> {
-    consecutive_prime_limit_search(53, 1.0, 20)
-}
-
 #[wasm_bindgen]
 pub fn consecutive_prime_limit_search(
     prime_cap: Harmonic,
-    ek: Cents,
+    ek_adjusted: Cents,
     n_results: usize,
 ) -> Result<(), JsValue> {
     let window = web_sys::window().expect("no window");
     let document = window.document().expect("no document");
 
     let limit = PrimeLimit::new(prime_cap);
+    let ek =
+        ek_adjusted * 12e2 / limit.pitches.last().expect("no harmonics");
     // This is shamelessly coupled to the HTML
     let table = document.get_element_by_id("equal-temperaments").unwrap_or({
         // If there's no matching table, let's make one!
