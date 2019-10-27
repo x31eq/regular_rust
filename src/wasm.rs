86ec50969e0f73f48f7feae7819c2e66741747e1
Graham Breed <gbreed@gmail.com>
2019-10-27 14:25:14 +0000

    Show rank 2 results in Wasm

--- src/wasm.rs
+++ src/wasm.rs
@@ -34,13 +34,44 @@ pub fn consecutive_prime_limit_search(
         row.append_child(&cell)?;
     }
     table.append_child(&row)?;
-    for et in cangwu::get_equal_temperaments(&limit.pitches, ek, n_results) {
+    let mappings = cangwu::get_equal_temperaments(
+                            &limit.pitches, ek, n_results);
+    for et in mappings.iter() {
         let row = document.create_element("tr")?;
         for element in et {
             let cell = document.create_element("td")?;
             cell.set_text_content(Some(&element.to_string()));
             row.append_child(&cell)?;
         }
+        table.append_child(&row)?;
+    }
+
+    // Now make another table for the next lot of results
+    let table = document.create_element("table")?;
+    div.append_child(&table)?;
+    table.set_inner_html("");
+    let row = document.create_element("tr")?;
+    let cell = document.create_element("th")?;
+    cell.set_text_content(Some("Rank 2"));
+    row.append_child(&cell)?;
+    table.append_child(&row)?;
+    let mut rts = Vec::with_capacity(mappings.len());
+    for mapping in mappings.iter() {
+        rts.push(vec![mapping.clone()]);
+    }
+    let new_rts = cangwu::higher_rank_search(
+        &limit.pitches,
+        &mappings,
+        &rts,
+        ek,
+        // Note: no safety
+        n_results);
+    for rt in new_rts {
+        let row = document.create_element("tr")?;
+        let cell = document.create_element("td")?;
+        let text = format!("{} & {}", rt[0][0], rt[1][0]);
+        cell.set_text_content(Some(&text));
+        row.append_child(&cell)?;
         table.append_child(&row)?;
     }
     Ok(())
