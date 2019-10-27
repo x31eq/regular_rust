1b38e023bc96c2e3b06a7a710f2d1afc2b5685b2
Graham Breed <gbreed@gmail.com>
2019-10-27 14:56:16 +0000

    Ran cargo fmt

--- src/wasm.rs
+++ src/wasm.rs
@@ -1,7 +1,7 @@
 use wasm_bindgen::prelude::{wasm_bindgen, JsValue};
 
 use super::cangwu;
-use super::{Cents, Harmonic, FactorElement, PrimeLimit, join};
+use super::{join, Cents, FactorElement, Harmonic, PrimeLimit};
 
 #[wasm_bindgen]
 pub fn consecutive_prime_limit_search(
@@ -14,17 +14,18 @@ pub fn consecutive_prime_limit_search(
 
     let limit = PrimeLimit::new(prime_cap);
     let dimension = limit.pitches.len();
-    let ek =
-        ek_adjusted * 12e2 / limit.pitches.last().expect("no harmonics");
+    let ek = ek_adjusted * 12e2 / limit.pitches.last().expect("no harmonics");
     let safety = 4 * (dimension as f64).sqrt().floor() as usize;
     // This is shamelessly coupled to the HTML
-    let div = document.get_element_by_id("regular-temperaments").unwrap_or({
-        // If there's no matching table, let's make one!
-        let div = document.create_element("div")?;
-        div.set_id("regular-temperaments");
-        document.body().expect("no body").append_child(&div)?;
-        div
-    });
+    let div = document
+        .get_element_by_id("regular-temperaments")
+        .unwrap_or({
+            // If there's no matching table, let's make one!
+            let div = document.create_element("div")?;
+            div.set_id("regular-temperaments");
+            document.body().expect("no body").append_child(&div)?;
+            div
+        });
     div.set_inner_html("");
     let table = document.create_element("table")?;
     div.append_child(&table)?;
@@ -37,7 +38,10 @@ pub fn consecutive_prime_limit_search(
     }
     table.append_child(&row)?;
     let mappings = cangwu::get_equal_temperaments(
-                            &limit.pitches, ek, n_results + safety);
+        &limit.pitches,
+        ek,
+        n_results + safety,
+    );
     for et in mappings.iter().take(n_results) {
         let row = document.create_element("tr")?;
         for element in et {
@@ -70,12 +74,14 @@ pub fn consecutive_prime_limit_search(
             &mappings,
             &rts,
             ek,
-            eff_n_results);
+            eff_n_results,
+        );
 
         for rt in rts.iter().take(n_results) {
             let row = document.create_element("tr")?;
             let cell = document.create_element("td")?;
-            let octaves: Vec<FactorElement> = rt.iter().map(|m| m[0]).collect();
+            let octaves: Vec<FactorElement> =
+                rt.iter().map(|m| m[0]).collect();
             let text = join(" & ", &octaves);
             cell.set_text_content(Some(&text));
             row.append_child(&cell)?;
