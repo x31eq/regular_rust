682c42a9cb51e1543e9310bb2d878df16ef9d4ee
Graham Breed <gbreed@gmail.com>
2024-03-29 20:27:00 +0000

    Function for unison vectors

--- src/uv.rs
+++ src/uv.rs
@@ -1,19 +1,27 @@
 extern crate nalgebra as na;
 use na::{DMatrix};
 
+use super::{ETMap, Exponent, Mapping};
+
+/// Return the commatic unison vector for a mapping with only one dimension short
+pub fn only_unison_vector(mapping: Mapping) -> Option<ETMap> {
+    let rank = mapping.len();
+    if rank == 0 {
+        return None;
+    }
+    let dimension = mapping[0].len();
+    let fiter = mapping.iter().flat_map(|m| m.iter()).map(|&x| x as f64);
+    let fmap = DMatrix::from_iterator(dimension, rank, fiter);
+    let mut sq = fmap.insert_column(0, 0.0);
+    sq[(0, 0)] = 1.0;
+    let det = sq.clone().determinant();
+    let adjoint = sq.clone().try_inverse()? * det;
+    Some(adjoint.row(0).iter().map(|&x| x as Exponent).collect())
+}
+
 #[test]
 fn meantone5() {
-    let mapping = DMatrix::from_vec(3, 2, vec![12, 19, 28, 7, 11, 16]);
-    let mut squared = mapping.clone().insert_column(0, 0);
-    squared[(0, 0)] = 1;
-    let expected =
-        DMatrix::from_vec(3, 3, vec![1, 0, 0, 12, 19, 28, 7, 11, 16]);
-    assert_eq!(expected, squared);
-    let convert_float = squared.iter().cloned().map(|x| x as f64);
-    let fsquare = DMatrix::from_iterator(3, 3, convert_float);
-    let inverted = fsquare.clone().try_inverse().expect("no inverse")
-        * fsquare.clone().determinant();
-    let uv: Vec<_> = inverted.row(0).iter().map(|&x| x as i64).collect();
+    let mapping = vec![vec![12, 19, 28], vec![7, 11, 16]];
     let expected = vec![-4, 4, -1];
-    assert_eq!(expected, uv);
+    assert_eq!(Some(expected), only_unison_vector(mapping));
 }
