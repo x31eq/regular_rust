3e643500ccd876e316be14aadcc786662d7d4295
Graham Breed <gbreed@gmail.com>
2024-03-30 14:36:27 +0000

    Stub lookup of names

---
+++ src/names.rs
@@ -0,0 +1,15 @@
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+
+use super::ETMap;
+
+lazy_static! {
+    pub static ref NAMES_BY_LIMIT: HashMap<Vec<&'static str>, HashMap<ETMap, &'static str>> =
+        HashMap::from([(
+            vec!["2", "5", "11"],
+            HashMap::from([
+                (vec![1, 3, 2, 0, -7], "Wizz"),
+                (vec![1, 1, 0, 1, 0, 0], "2.5.11-limit JI"),
+            ]),
+        )]);
+}
