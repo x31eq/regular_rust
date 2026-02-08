4f79e7acf3e276c38844bd1f4b95e4db03456abb
Graham Breed <gbreed@gmail.com>
2026-02-08 11:13:42 +0000

    Removed redundant test

--- src/te.rs
+++ src/te.rs
@@ -332,16 +332,6 @@ fn pote_mistunings() {
     assert_eq!(fmt_tuning, expected.to_string());
 }
 
-#[test]
-fn marvel_from_name() {
-    let limit11 = super::PrimeLimit::new(11);
-    let expected = make_marvel(&limit11);
-    let from_name = TETemperament::from_name(&limit11, "22 & 31 & 41")
-        .expect("couldn't make marvel from name");
-    assert_eq!(expected.melody, from_name.melody);
-    assert_eq!(expected.plimit, from_name.plimit);
-}
-
 #[rustfmt::skip]
 #[test]
 fn mystery() {
